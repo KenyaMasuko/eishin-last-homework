ff1bd07d6f79f25c82caa9f05d779ff71370498e
kenya12814 <85816730+KenyaMasuko@users.noreply.github.com>
2023-01-23 22:17:12 +0900

    feat: 業界一覧CRUD実装

---
+++ app/Http/Controllers/Admin/IndustryController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Industry;
+use Illuminate\Http\Request;
+
+class IndustryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $industries = Industry::all();
+
+        return view('admin.industry.index', compact('industries'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.industry.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:20'
+        ]);
+
+        $Industry = new Industry();
+        $Industry->name = $request->name;
+        $Industry->save();
+
+        return redirect(route('admin.industry.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Industry  $industry
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Industry $industry)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Industry  $industry
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Industry $industry)
+    {
+        $industries = Industry::find($industry->id);
+
+        return view('admin.industry.edit', compact('industries'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Industry  $industry
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Industry $industry)
+    {
+        $request->validate([
+            'name' => 'required|max:20'
+        ]);
+
+        $industries = $request->all();
+        unset($industries['_token']);
+        unset($industries['_method']);
+        Industry::where(['id' => $industry['id']])->update($industries);
+
+        return redirect(route('admin.industry.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Industry  $industry
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Industry $industry)
+    {
+        Industry::find($industry->id)->delete();
+
+        return redirect(route('admin.industry.index'));
+    }
+}
