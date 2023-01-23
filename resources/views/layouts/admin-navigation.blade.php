184482c89efa6b8021c46b107906a5e646bc0fb8
kenya12814 <85816730+KenyaMasuko@users.noreply.github.com>
2023-01-23 22:17:12 +0900

    feat: 業界一覧CRUD実装

--- resources/views/layouts/admin-navigation.blade.php
+++ resources/views/layouts/admin-navigation.blade.php
@@ -1,8 +1,96 @@
-<nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+<div class="min-h-screen bg-gray-100 absolute left-0 top-0">
+    <div class="sidebar min-h-screen  overflow-hidden border-r w-56 hover:bg-white hover:shadow-lg">
+        <div class="flex h-screen flex-col justify-between pt-2 pb-6">
+            <div>
+                <ul class="mt-6 space-y-2 tracking-wide">
+                    <li class="min-w-max">
+                        <a href="{{ route('admin.dashboard') }}" aria-label="dashboard"
+                            class="relative flex items-center space-x-4 bg-gradient-to-r px-4 py-3 text-gray-600">
+                            <svg class="-ml-1 h-6 w-6" viewBox="0 0 24 24" fill="none">
+                                <path
+                                    d="M6 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V8ZM6 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-1Z"
+                                    class="fill-current text-cyan-400 dark:fill-slate-600"></path>
+                                <path d="M13 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2V8Z"
+                                    class="fill-current text-cyan-200 group-hover:text-cyan-300"></path>
+                                <path d="M13 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-1Z"
+                                    class="fill-current group-hover:text-sky-300"></path>
+                            </svg>
+                            <span class="-mr-1 font-medium">ダッシュボード</span>
+                        </a>
+                    </li>
+                    <li class="min-w-max">
+                        <a href="{{ route('admin.industry.index') }}"
+                            class="bg group flex items-center space-x-4 rounded-full px-4 py-3 text-gray-600">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
+                                fill="currentColor">
+                                <path class="fill-current text-gray-300 group-hover:text-cyan-300" fill-rule="evenodd"
+                                    d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z"
+                                    clip-rule="evenodd" />
+                                <path class="fill-current text-gray-600 group-hover:text-cyan-600"
+                                    d="M6 12a2 2 0 012-2h8a2 2 0 012 2v2a2 2 0 01-2 2H2h2a2 2 0 002-2v-2z" />
+                            </svg>
+                            <span class="group-hover:text-gray-700">業界一覧</span>
+                        </a>
+                    </li>
+                    <li class="min-w-max">
+                        <a href="#" class="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
+                                fill="currentColor">
+                                <path class="fill-current text-gray-600 group-hover:text-cyan-600" fill-rule="evenodd"
+                                    d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
+                                    clip-rule="evenodd" />
+                                <path class="fill-current text-gray-300 group-hover:text-cyan-300"
+                                    d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z" />
+                            </svg>
+                            <span class="group-hover:text-gray-700">特徴一覧</span>
+                        </a>
+                    </li>
+                    <li class="min-w-max">
+                        <a href="#" class="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
+                                fill="currentColor">
+                                <path class="fill-current text-gray-600 group-hover:text-cyan-600"
+                                    d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z" />
+                                <path class="fill-current text-gray-300 group-hover:text-cyan-300"
+                                    d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z" />
+                            </svg>
+                            <span class="group-hover:text-gray-700">企業一覧</span>
+                        </a>
+                    </li>
+                    <li class="min-w-max">
+                        <a href="#" class="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
+                                fill="currentColor">
+                                <path class="fill-current text-gray-300 group-hover:text-cyan-300"
+                                    d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z" />
+                                <path class="fill-current text-gray-600 group-hover:text-cyan-600" fill-rule="evenodd"
+                                    d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                            <span class="group-hover:text-gray-700">求人一覧</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="w-max -mb-3">
+                <a href="#" class="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 group-hover:fill-cyan-600"
+                        viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd"
+                            d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
+                            clip-rule="evenodd" />
+                    </svg>
+                    <span class="group-hover:text-gray-700">Settings</span>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<nav x-data="{ open: false }" class=" ml-56 bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
-            <div class="flex">
+        <div class="flex h-16 justify-end">
+            {{-- <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('admin.dashboard') }}">
@@ -16,7 +104,7 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
-            </div>
+            </div> --}}
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
