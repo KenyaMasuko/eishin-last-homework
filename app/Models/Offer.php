7162724a8f9a26e018789fc09252042d712642b8
kenya12814 <85816730+KenyaMasuko@users.noreply.github.com>
2023-01-25 22:35:50 +0900

    feat: 求人と特徴のモデルを定義

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -5,18 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Feature extends Model
+class Offer extends Model
 {
     use HasFactory;
-    /**
-     * 複数代入しない属性
-     *
-     * @var array
-     */
+
     protected $guarded = ['id']; //複数代入しない属性を指定
 
-    public function offers()
+    public function features()
     {
-        $this->belongsToMany(Offer::class);
+        $this->belongsToMany(Feature::class);
     }
 }
