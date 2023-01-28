2de401816433b1033a63d50fff8df557bcf6259f
kenya12814 <85816730+KenyaMasuko@users.noreply.github.com>
2023-01-28 09:51:46 +0900

    feat: 求人用のテーブルを定義

---
+++ app/Http/Controllers/Company/OfferController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Offer;
+use Illuminate\Http\Request;
+
+class OfferController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $offers = Offer::all();
+
+        return view('company.offer.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('company.offer.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validatedOffer = $request->validate([
+            'title' => 'required:20',
+            'description' => 'required:max255',
+            'is_public' => 'require'
+        ]);
+
+        $Offer = new Offer();
+        $Offer->title = $validatedOffer['title'];
+        $Offer->description = $validatedOffer['description'];
+        $Offer->is_public = $validatedOffer['is_public'];
+
+        $Offer->save();
+
+        $Offer->features()->sync($validatedOffer['feature_ids']);
+
+        return redirect(route('company.offer.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Offer  $offer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Offer $offer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Offer  $offer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Offer $offer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Offer  $offer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Offer $offer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Offer  $offer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Offer $offer)
+    {
+        //
+    }
+}
